d77b2f8757dce820012f45cfbdc93162de247099
Smith <adam.smith03@cardinalhealth.com>
2020-12-28 13:46:40 -0500

    updated main.py

--- source_code/main.py
+++ source_code/main.py
@@ -1,23 +1,15 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello! This is the main page <h1>HELLO<h1>'
+    return render_template('index.html')
 
-@app.route('/<name>')
-def user(name):
-    return f'Hello {name}!'
-
-
-@app.route('/admin')
-def admin():
-    return redirect(url_for('home'))
-
-
+@app.route('/test/')
+def test():
+    return render_template('new.html',)
 
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
